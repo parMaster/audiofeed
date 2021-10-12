ef71954b2f341b3eb4da4634c44c969fae4b11d5
parMaster <tony.racomore@gmail.com>
2021-10-12 16:27:16 +0300

    A bit of refactoring. Decomposed a main struct to separate entities: - FeedServer for handlers - Title to read content

--- main.go
+++ main.go
@@ -14,10 +14,78 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func (Feed *feed) readMediaFolder() []string {
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type feedServer struct {
+	HostName    string
+	MediaFolder string
+	Port        string
+	Title       title
+}
+
+type title struct {
+	Name      string
+	Path      string
+	CoverPath string
+	Chapters  []string
+}
+
+func (FeedServer *feedServer) index(w http.ResponseWriter, r *http.Request) {
+	rw := NewResponse()
+
+	titlesTemplate := template.New("Title with chapters")
+	titlesTemplate.Parse(titlesTemplateBody)
+
+	titlesTemplate.Execute(rw, FeedServer.Title.fromMediaFolder(FeedServer.MediaFolder))
+	w.Write([]byte(rw.body))
+}
+
+func (FeedServer *feedServer) title(w http.ResponseWriter, r *http.Request) {
+
+	rw := NewResponse()
+
+	xmlTemplate := template.New("Title with chapters")
+	xmlTemplate.Parse(xmlTemplateBody)
+
+	params := mux.Vars(r)
+	FeedServer.HostName = r.Host
+	FeedServer.Title.Name = params["name"]
+	FeedServer.Title.Path = filepath.Join("title", FeedServer.Title.Name)
+
+	FeedServer.Title.readTitle(filepath.Join(FeedServer.MediaFolder, FeedServer.Title.Name))
+
+	err := xmlTemplate.Execute(rw, FeedServer)
+	check(err)
+
+	w.Header().Add("Content-Type", "text/xml; charset=utf-8")
+	w.Write([]byte(rw.body))
+}
+
+func (FeedServer *feedServer) info(w http.ResponseWriter, r *http.Request) {
+	b := fmt.Sprintf("%v", r)
+	w.Write([]byte(b))
+}
+
+func (FeedServer *feedServer) stylesheet(w http.ResponseWriter, r *http.Request) {
+	b, err := os.ReadFile("feed.xsl")
+	check(err)
+	w.Write([]byte(b))
+}
+
+func (FeedServer *feedServer) readCmdParams() {
+	flag.StringVar(&FeedServer.Port, "port", "8080", "Server port")
+	flag.StringVar(&FeedServer.MediaFolder, "folder", "audio", "Name of a folder with media")
+	flag.Parse()
+}
+
+func (Title *title) fromMediaFolder(mediaFolder string) []string {
 	var titles []string
 
-	eTitles, err := filepath.Glob("./" + Feed.MediaFolder + "/*")
+	eTitles, err := filepath.Glob(filepath.Join(mediaFolder, "/*"))
 	check(err)
 
 	for _, t := range eTitles {
@@ -27,45 +95,13 @@ func (Feed *feed) readMediaFolder() []string {
 	return titles
 }
 
-func (Feed *feed) index(w http.ResponseWriter, r *http.Request) {
-	rw := NewResponse()
-
-	titlesTemplate := template.New("Title with chapters")
-	titlesTemplate.Parse(titlesTemplateBody)
-
-	titlesTemplate.Execute(rw, Feed.readMediaFolder())
-	w.Write([]byte(rw.body))
-}
-
-type feed struct {
-	HostName    string
-	MediaFolder string
-	ServerPort  string
-	TitleName   string
-	TitlePath   string
-	CoverPath   string
-	Chapters    []string
-}
-
-// type feedServer struct {
-// 	HostName    string
-// 	MediaFolder string
-// 	ServerPort  string
-// }
-
-// type feed struct {
-// 	TitleName   string
-// 	TitlePath   string
-// 	CoverPath   string
-// }
-
-func (Feed *feed) readTitle() {
+func (Title *title) readTitle(titlePath string) {
 
 	var isChapter = regexp.MustCompile(`(?im)\.(mp3|m4a|m4b)$`)
 	var isCover = regexp.MustCompile(`(?im)\.(jpg|jpeg|png)$`)
 
 	var chapters []string
-	err := filepath.WalkDir("./"+Feed.MediaFolder+"/"+Feed.TitleName+"/", func(path string, entry fs.DirEntry, err error) error {
+	err := filepath.WalkDir(titlePath, func(path string, entry fs.DirEntry, err error) error {
 		check(err)
 
 		if !entry.IsDir() {
@@ -73,75 +109,32 @@ func (Feed *feed) readTitle() {
 				chapters = append(chapters, path)
 				log.Println("visited: ", path)
 			} else if isCover.MatchString(path) {
-				Feed.CoverPath = path
-				log.Println("cover found: ", Feed.CoverPath)
+				Title.CoverPath = path
+				log.Println("cover found: ", Title.CoverPath)
 			}
 		}
 		return nil
 	})
 	check(err)
 
-	Feed.Chapters = chapters
-}
-
-func (Feed *feed) title(w http.ResponseWriter, r *http.Request) {
-
-	rw := NewResponse()
-
-	xmlTemplate := template.New("Title with chapters")
-	xmlTemplate.Parse(xmlTemplateBody)
-
-	params := mux.Vars(r)
-	Feed.TitleName = params["name"]
-	Feed.HostName = r.Host
-	Feed.TitlePath = "/title/" + Feed.TitleName
-
-	Feed.readTitle()
-
-	xmlTemplate.Execute(rw, Feed)
-
-	w.Header().Add("Content-Type", "text/xml; charset=utf-8")
-	w.Write([]byte(rw.body))
-}
-
-func info(w http.ResponseWriter, r *http.Request) {
-	b := fmt.Sprintf("%v", r)
-	w.Write([]byte(b))
-}
-
-func stylesheet(w http.ResponseWriter, r *http.Request) {
-	b, err := os.ReadFile("./feed.xsl")
-	check(err)
-	w.Write([]byte(b))
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func (Feed *feed) readCmdParams() {
-	flag.StringVar(&Feed.ServerPort, "port", "8080", "Server port")
-	flag.StringVar(&Feed.MediaFolder, "folder", "audio", "Name of a folder with media")
-	flag.Parse()
+	Title.Chapters = chapters
 }
 
 func main() {
-	var Feed feed
+	var FeedServer feedServer
 
 	r := mux.NewRouter()
-	r.HandleFunc("/index", Feed.index).Methods("GET")
-	r.HandleFunc("/info", info).Methods("GET")
-	r.HandleFunc("/feed.xsl", stylesheet).Methods("GET")
-	r.HandleFunc("/title/{name}", Feed.title).Methods("GET")
+	r.HandleFunc("/index", FeedServer.index).Methods("GET")
+	r.HandleFunc("/info", FeedServer.info).Methods("GET")
+	r.HandleFunc("/feed.xsl", FeedServer.stylesheet).Methods("GET")
+	r.HandleFunc("/title/{name}", FeedServer.title).Methods("GET")
 
-	Feed.readCmdParams()
+	FeedServer.readCmdParams()
 
 	http.Handle("/", r)
-	http.Handle("/"+Feed.MediaFolder+"/", http.StripPrefix("/"+Feed.MediaFolder+"/", http.FileServer(http.Dir("./"+Feed.MediaFolder))))
+	http.Handle("/"+FeedServer.MediaFolder+"/", http.StripPrefix("/"+FeedServer.MediaFolder+"/", http.FileServer(http.Dir(FeedServer.MediaFolder))))
 
 	log.Println("Listening...")
-	err := http.ListenAndServe(":"+Feed.ServerPort, nil)
+	err := http.ListenAndServe(":"+FeedServer.Port, nil)
 	check(err)
 }
--- templates.go
+++ templates.go
@@ -4,37 +4,37 @@ const xmlTemplateBody = `<?xml version="1.0" ?>
 <?xml-stylesheet href="/feed.xsl" type="text/xsl"?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:media="http://search.yahoo.com/mrss/" xmlns:creativeCommons="http://backend.userland.com/creativeCommonsRssModule" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 	<channel>
-		<title><![CDATA[{{.TitleName}}]]></title>
-		<link><![CDATA[http://{{.HostName}}{{.TitlePath}}]]></link>
-		<atom:link rel="self" href="http://{{.HostName}}{{.TitlePath}}" />
-		<description><![CDATA[{{.TitleName}}]]></description>
+		<title><![CDATA[{{.Title.Name}}]]></title>
+		<link><![CDATA[http://{{.HostName}}/{{.Title.Path}}]]></link>
+		<atom:link rel="self" href="http://{{.HostName}}/{{.Title.Path}}" />
+		<description><![CDATA[{{.Title.Name}}]]></description>
 		<itunes:type>serial</itunes:type>
 		<itunes:author>AudioFeed</itunes:author>
-		<itunes:summary><![CDATA[{{.TitleName}}]]></itunes:summary>
+		<itunes:summary><![CDATA[{{.Title.Name}}]]></itunes:summary>
 		<itunes:owner>
 		<itunes:name>Audiofeed</itunes:name>
 		<itunes:email>af@af.com</itunes:email>
 		</itunes:owner>
 		<itunes:category text="AudioBooks" />
 
-		{{if .CoverPath}}
-		<itunes:image href="http://{{.HostName}}/{{.CoverPath}}" />
-		<media:thumbnail url="http://{{.HostName}}/{{.CoverPath}}" />
+		{{if .Title.CoverPath}}
+		<itunes:image href="http://{{.HostName}}/{{.Title.CoverPath}}" />
+		<media:thumbnail url="http://{{.HostName}}/{{.Title.CoverPath}}" />
 		<image>
-			<title>{{.TitleName}}</title>
-			<url>http://{{.HostName}}/{{.CoverPath}}</url>
-			<link>http://{{.HostName}}{{.TitlePath}}</link>
+			<title>{{.Title.Name}}</title>
+			<url>http://{{.HostName}}/{{.Title.CoverPath}}</url>
+			<link>http://{{.HostName}}/{{.Title.Path}}</link>
 		</image>
 		{{end}}
 
-		{{ $TitleName := .TitleName }}
+		{{ $TitleName := .Title.Name }}
 		{{ $HostName := .HostName }}
-		{{ $TitlePath := .TitlePath }}
-		{{ range $k, $v := .Chapters }}
+		{{ $TitlePath := .Title.Path }}
+		{{ range $k, $v := .Title.Chapters }}
 		<item>
 			<title><![CDATA[{{$TitleName}}]]></title>
 			<itunes:episode><![CDATA[{{$k}}]]></itunes:episode>
-			<link><![CDATA[http://{{$HostName}}{{$TitlePath}}]]></link>
+			<link><![CDATA[http://{{$HostName}}/{{$TitlePath}}]]></link>
 			<enclosure url="http://{{$HostName}}/{{$v}}" length="0" type="audio/mpeg" />
 			<itunes:explicit>No</itunes:explicit>
 			<itunes:block>No</itunes:block>
